264ad36f9120c9c1604df813e48d03cabc677942
NpmMaster <167225000+NpmMaster@users.noreply.github.com>
2024-04-23 15:53:24 +0000

    Added test.js And developing table mode

---
+++ test.js
@@ -0,0 +1,4 @@
+const { Table } = require('./wnc_utils/util_table.js')
+
+app = new Table('item1 item2 item3',color='blue')
+print(app)
--- wnc_utils/util_table.js
+++ wnc_utils/util_table.js
@@ -3,13 +3,15 @@ const { App } = require('./util.js')
 
 
 class Table {
-  process.argv.ForEach(constructor(*items, color) {
-    this._items=items
+  constructor(items, color) {
+    this._items=process.argv.forEach(items)
     this._color=color
     try {
       divi = this._items.split()
       dividez = divi.join('  ')
       app = new App(dividez, option=this._color)
-    } catch((error) => {console.log('Error while running table mode')}
+    } catch {console.log('Error while running Table mode')}
   }
 }
+
+module.exports = Table
