ae27ad5396991d374d9b3e707fa84def791b35f2
NpmMaster <167225000+NpmMaster@users.noreply.github.com>
2024-04-23 12:42:37 -0300

    Updated util_table.js (Forgor to make .foreach bruh)

--- wnc_utils/util_table.js
+++ wnc_utils/util_table.js
@@ -1,5 +1,15 @@
+const { App } = require('./util.js')
+
+
+
 class Table {
-  constructor(items: []) {
-    
+  process.argv.ForEach(constructor(*items, color) {
+    this._items=items
+    this._color=color
+    try {
+      divi = this._items.split()
+      dividez = divi.join('  ')
+      app = new App(dividez, option=this._color)
+    } catch((error) => {console.log('Error while running table mode')}
   }
 }
